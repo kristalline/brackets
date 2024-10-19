8cca236d5f42215b5a85cbb252cc346b84521834
kristalline <174002622+kristalline@users.noreply.github.com>
2024-10-19 18:28:25 +0700

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 1) return false;
+  const brackets = Object.fromEntries(bracketsConfig);
+
+  const stack = str.split('').reduce((acc, el) => {
+    if (brackets[acc.at(-1)] === el) {
+      acc.pop();
+    } else if (Object.hasOwn(brackets, el)) {
+      acc.push(el);
+    }
+    return acc;
+  }, []);
+  return !stack.length;
 }
